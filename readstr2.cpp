8093274b1690b0c4c8a59dc3f5acb733bc4ad55d
AhnByeoungHo <37297879+AhnByeoungHo@users.noreply.github.com>
2018-03-23 16:29:58 +0900

    201424488안병호 과제2
    각각의 시작부분에 Student Course Courseregistration에 적당한 시그널을 주고 데이터멤버간의 구분시그널은 '|'을 사용하여 출력하였습니다 출력시 끝나는 부분은'%'로통일하여 클래스간 헷갈림이 없도록 하였습니다.

---
+++ readstr2.cpp
@@ -0,0 +1,253 @@
+#include<iostream>
+#include <fstream>
+#include<string>
+using namespace std;
+
+const int MaxBufferSize = 200;
+class Student
+{
+public:
+	// data members
+	char* id, *address, *first_enroll, *credit_hour;
+	// method
+	Student() {
+		id = new char[20];
+		address = new char[20];
+		first_enroll = new char[20];
+		credit_hour = new char[20];
+	}
+	// default constructor
+	Student(char* _id, char* _address, char* _first_enroll, char* _credit_hour) :
+		id(_id), address(_address), first_enroll(_first_enroll), credit_hour(_credit_hour) {}
+	Student& changeValue(char* _id, char* _address, char* _first_enroll, char* _credit_hour) {
+		id = _id;
+		address = _address;
+		first_enroll = _first_enroll;
+		credit_hour = _credit_hour;
+	}
+	Student operator=(Student from) {
+		this->id = from.id;
+		this->address = from.address;
+		this->first_enroll = from.first_enroll;
+		this->credit_hour = from.credit_hour;
+
+		return *this;
+	}
+	friend istream & operator >> (istream & is, Student & s);
+	friend ostream & operator << (ostream & os, Student & s);
+};
+
+
+istream & operator >> (istream & is, Student & s)
+{ // read fields from 
+	
+	cout << "(Student)Enter<y> continue, or <Enter> to end: " << flush;
+	is.getline(s.id, 30);
+	if (strlen(s.id) == 0) return is;
+	cout << "Enter id: " << flush; is.getline(s.id, 30);
+	cout << "Enter address: " << flush; is.getline(s.address, 30);
+	cout << "Enter first enroll: " << flush; is.getline(s.first_enroll, 30);
+	cout << "Enter credit hour: " << flush; is.getline(s.credit_hour, 30);
+	return is;
+	
+}
+ostream & operator << (ostream & os, Student & s)
+{ // insert fields into file
+	os <<'$' << s.id << '|' << s.address << '|' << s.first_enroll
+		<< '|' << s.credit_hour << '%';
+	return os;
+}
+
+
+class CourseRegistration
+{
+public:
+	// data members
+	char* course_id, *student_id, *course_grade, *credit_hour;
+	// method
+	CourseRegistration() {
+		course_id = new char[20];
+		student_id = new char[20];
+		course_grade = new char[20];
+		credit_hour = new char[20];
+	}
+	// default constructor
+	CourseRegistration(char* _course_id, char* _student_id, char* _course_grade, char* _credit_hour) :
+		course_id(_course_id), student_id(_student_id), course_grade(_course_grade), credit_hour(_credit_hour) {}
+	CourseRegistration& changeValue(char* _course_id, char* _student_id, char* _course_grade, char* _credit_hour) {
+		course_id = _course_id;
+		student_id= _student_id;
+		course_grade = _course_grade;
+		credit_hour = _credit_hour;
+
+	}
+	CourseRegistration operator=(CourseRegistration from) {
+		this->course_id = from.course_id;
+		this->student_id = from.student_id;
+		this->course_grade = from.course_grade;
+		this->credit_hour = from.credit_hour;
+
+		return *this;
+	}
+	friend istream & operator >> (istream & is, CourseRegistration & s);
+	friend ostream & operator << (ostream & os, CourseRegistration & s);
+};
+
+
+istream & operator >> (istream & is, CourseRegistration & s)
+{ // read fields from 
+	cout << "(CourseRegistration)Enter<y> continue, or <Enter> to end: " << flush;
+	is.getline(s.course_id, 30);
+	if (strlen(s.course_id) == 0) return is;
+	cout << "Enter course id: " << flush; is.getline(s.course_id, 30);
+	cout << "Enter student id: " << flush; is.getline(s.student_id, 30);
+	cout << "Enter course grade: " << flush; is.getline(s.course_grade, 30);
+	cout << "Enter credit hour: " << flush; is.getline(s.credit_hour, 30);
+	return is;
+}
+ostream & operator << (ostream & os, CourseRegistration & s)
+{ // insert fields into file
+	os <<'#' << s.course_id << '|' << s.student_id << '|' << s.course_grade
+		<< '|' << s.credit_hour << '%';
+	return os;
+}
+class Course
+{
+public:
+	// data members
+	char* course_id, *course_name, *course_grade,
+		*course_hour,*teacher,*classroom;
+	// method
+	Course() {
+		course_id = new char[20];
+		course_name = new char[20];
+		course_grade = new char[20];
+		course_hour = new char[20];
+		teacher = new char[20];
+		classroom = new char[20];
+	}
+	// default constructor
+	Course(char* _course_id, char* _course_name, char* _course_grade, char* _course_hour
+	,char* _teacher,char* _classroom) :
+		course_id(_course_id), course_name(_course_name), course_grade(_course_grade), course_hour(_course_hour)
+	,teacher(_teacher), classroom(_classroom){}
+	Course& changeValue(char* _course_id, char* _course_name, char* _course_grade, char* _course_hour
+		, char* _teacher, char* _classroom) {
+		course_id = _course_id;
+		course_name = _course_name;
+		course_grade = _course_grade;
+		course_hour = _course_hour;
+		teacher = _teacher;
+		classroom = _classroom;
+	}
+	Course operator=(Course from) {
+		this->course_id = from.course_id;
+		this->course_name = from.course_name;
+		this->course_grade = from.course_grade;
+		this->course_hour = from.course_hour;
+		this->teacher = from.teacher;
+		this->classroom = from.classroom;
+
+		return *this;
+	}
+	friend istream & operator >> (istream & is, Course & s);
+	friend ostream & operator << (ostream & os, Course & s);
+	
+};
+istream & operator >> (istream & is, Course & s)
+{ // read fields from 
+	cout << "(Course)Enter<y> continue, or <Enter> to end: " << flush;
+	is.getline(s.course_id, 30);
+	if (strlen(s.course_id) == 0) return is;
+	cout << "Enter course id: " << flush; is.getline(s.course_id, 30);
+	cout << "Enter student id: " << flush; is.getline(s.course_name, 30);
+	cout << "Enter course grade: " << flush; is.getline(s.course_grade, 30);
+	cout << "Enter credit hour: " << flush; is.getline(s.course_hour, 30);
+	cout << "Enter teacher: " << flush; is.getline(s.teacher, 30);
+	cout << "Enter classroom: " << flush; is.getline(s.classroom, 30);
+	return is;
+}
+ostream & operator << (ostream & os, Course & s)
+{ // insert fields into file
+	os << '@' << s.course_id <<'|'<< s.course_name << '|' << s.course_grade
+		<< '|' << s.course_hour << '|' << s.teacher << '|' << s.classroom << '%';
+	return os;
+}
+void read(char filename[20]) {
+	char ch;
+	fstream file; // declare fstream unattached
+	
+	file.open(filename, ios::in);			// Step 3 
+	file.unsetf(ios::skipws);// include white space in read
+	cout << "reading file!!" << endl;
+	
+	while (1)
+	{
+		file >> ch;							// Step 4a 
+		if (file.fail()) break;
+
+		if (ch == '|')
+			cout << "\t";
+		else if (ch == '$')
+		{
+			cout << endl << "---------Student----------" << endl;
+			cout << "id\t" << "address\t" <<"first_enroll\t"<< "credit_hour" << endl<<endl;
+		}
+			
+		else if (ch == '#')
+		{
+			cout << endl << "----Courseregistration----" << endl;
+			cout << "course id\t" << "student id\t" << "course grade\t" << "credit_hour" << endl<<endl;
+		}
+		else if (ch == '@')
+		{
+			cout << endl << "----------Course-----------" << endl;
+			cout << "course id\t" << "student id\t" << "course grade\t" << "credit_hour\t"
+				<<"teacher\t"<<"classroom\t" << endl<<endl;
+		}
+		else if (ch == '%')
+			cout << endl;
+		else
+			cout << ch;					// Step 4b 
+	}
+	cout << endl;
+	file.close();							// Step 5'
+	
+}
+void write(char filename[20]) {
+	Student p;
+	CourseRegistration Cr;
+	Course c;
+	cin.getline(filename, 19);
+	ofstream ostream(filename, ios::out);
+	if (ostream.fail()) {
+		cout << "File open failed!" << endl;
+	}
+	while (1) {
+		cin >> p; // read fields of person
+		if (strlen(p.id) == 0) break;
+		// write person to output stream
+		ostream << p;
+	}
+	while (1) {
+		cin >> c;
+		if (strlen(c.course_id) == 0) break;
+		ostream << c;
+	}
+
+	while (1) {
+		cin >> Cr;
+		if (strlen(Cr.course_id) == 0) break;
+		ostream << Cr;
+	}
+	ostream.close();
+}
+
+int main() {
+	char filename[20];
+	cout << "Enter the file name:" << flush;
+	write(filename);
+	read(filename);
+	system("pause");
+}
+
